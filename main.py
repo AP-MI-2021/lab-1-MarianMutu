9fb787516b2b959fa6fb322b4ddd9c4a75a5b34c
MarianMutu <91467893+MarianMutu@users.noreply.github.com>
2021-09-29 15:42:17 +0300

    Add files via upload

--- main.py
+++ main.py
@@ -1,33 +1,58 @@
 '''
 Returneaza true daca n este prim si false daca nu.
 '''
+
+
 def is_prime(n):
-  # codul vostru aici
-  
-  
+    for i in range(n-1,2):
+        if n%i==0:
+             return False
+        return True
+
+
 '''
 Returneaza produsul numerelor din lista lst.
 '''
+
+
 def get_product(lst):
-  # codul vostru aici
-  
-  
+    p=1
+    for x in lst:
+      p=p*lst(x)
+    return p
 '''
 Returneaza CMMDC a doua numere x si y folosind primul algoritm.
 '''
+
+
 def get_cmmdc_v1(x, y):
-  # codul vostru aici
-  
-  
+    while x!=y:
+        if x>y:
+            x=x-y
+        else:
+            y=y-x
+
+    return x
 '''
 Returneaza CMMDC a doua numere x si y folosind al doilea algoritm.
 '''
+
 def get_cmmdc_v2(x, y):
-  # codul vostru aici
-  
-  
+    d=x+y
+    while d>0:
+        if x%d==0 and y%d==0:
+            return d
+        d-=1
+    return 1
+
+
 def main():
-  # interfata de tip consola aici
+
+    is_prime(78)
+    get_product(4)
+    get_cmmdc_v1(2,6)
+    get_cmmdc_v2(9,27)
+
 
 if __name__ == '__main__':
-  main()
+    main()
